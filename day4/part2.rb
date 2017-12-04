0b39488faae114bfaa4076d616b91360a2bfe680
Marina Chirchikova <marina.chirchikova@adgear.com>
2017-12-04 16:35:36 -0500

    Reorganize repo and add day 4

---
+++ day4/part2.rb
@@ -0,0 +1,24 @@
+def process(test = false)
+  input = test ? test_string : File.read('input.txt')
+  sum = 0
+  array = input.split("\n")
+
+  array.each do |phrase|
+    invalid = false
+    words = phrase.split(" ")
+    count = Hash.new(0)
+    phrase.scan(/\w+/) do |word|
+      count[word.chars.sort.join] += 1
+    end
+    invalid = count.any? {|k, v| v > 1}
+
+    sum += 1 unless invalid
+  end
+  sum
+end
+
+def test_string
+  "oiii ioii iioi iiio"
+end
+
+puts process
