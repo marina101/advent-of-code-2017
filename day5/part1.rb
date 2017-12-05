7a62fc0704a197772df3e4e86a61c2386770a152
Marina Chirchikova <marina.chirchikova@adgear.com>
2017-12-05 11:00:18 -0500

    Day 5

---
+++ day5/part1.rb
@@ -0,0 +1,21 @@
+def process(test = false)
+  input = test ? test_string : File.read('input.txt')
+  array = input.split("\n").collect{|i| i.to_i}
+  index = 0
+  steps = 0
+
+  while true do
+    old_index = index
+    index = index + array[index]
+    steps += 1
+    break if index >= array.length || index < 0
+    array[old_index] += 1
+  end
+  steps
+end
+
+def test_string
+  "0\n3\n0\n1\n-3"
+end
+
+puts process
